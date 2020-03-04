22107bb8b25f81d723eaab4cd6183eb54ddd91d1
shimobayashi <akimasashimobayashi@gmail.com>
2020-03-04 20:05:26 +0900

    Initial commit.

---
+++ index.ts
@@ -0,0 +1,12 @@
+// usage: npx ts-node index.ts
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+})();
