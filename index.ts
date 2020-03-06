5a4b6539f22bd5d22c051854bfaed3eb217c4541
shimobayashi <akimasashimobayashi@gmail.com>
2020-03-06 20:38:45 +0900

    フォロー新着作品の1つ目の画像詳細ページを開いて画像部分をスクショしてみた。

--- index.ts
+++ index.ts
@@ -6,38 +6,67 @@ import puppeteer from 'puppeteer';
 // 参考: https://qiita.com/rh_taro/items/32bb6851303cbc613124
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
-    slowMo: 0, //XXX 0だと勢いよくクロールしてしまうかも知れないので適当な値を入れたほうが良さそう(page.typeとか超遅くなるのでなんとかしたい)
+    headless: false, //XXX 後でtrueにする
+    slowMo: 500,
   });
   const page = await browser.newPage();
 
-  /* ログイン */
+  /*
+   * ログインする
+   */
   await page.goto('https://accounts.pixiv.net/login');
-  await page.type('#LoginComponent input[type=text]', process.env.PIXIV_USERNAME ?? '');
-  await page.type('#LoginComponent input[type=password]', process.env.PIXIV_PASSWORD ?? '');
+  // page.typeだとslowMoの分だけ待たされるので直接valueにぶち込む
+  await page.evaluate((credential) => {
+    document.querySelector('#LoginComponent input[type=text]')?.setAttribute('value', credential.username);
+    document.querySelector('#LoginComponent input[type=password]')?.setAttribute('value', credential.password);
+  }, {
+    username: process.env.PIXIV_USERNAME ?? '',
+    password: process.env.PIXIV_PASSWORD ?? '',
+  });
   page.click('#LoginComponent button[type=submit]');
-  //await page.waitForNavigation({timeout: 60000, waitUntil: 'domcontentloaded'});
-
+  // なんかダイアログが出るので一応消しておく
   // 参考: https://qiita.com/shora_kujira16/items/34cb4074dfa715007698
-  // 別にダイアログが出たまま操作しても問題ないかも知れない
   const xpath = `//button[text() = "わかった"]`;
-  await page.waitForXPath(xpath);
+  await page.waitForXPath(xpath); //XXX そのうちこのダイアログが出なくなると永遠に待つことになりそうなので、なんかタイムアウトを設定したい
   await (await page.$x(xpath))[0].click();
 
-  /* フォロー新着作品 */
-  await page.goto('https://www.pixiv.net/bookmark_new_illust.php');
-  // 参考: https://qiita.com/go_sagawa/items/85f97deab7ccfdce53ea
-  let figures = await page.$$('#js-mount-point-latest-following figure');
-  for (let figure of figures) {
-    // とりあえず試しに作品に関する情報とスクリーンショットを撮ってみる
-    for (let li of (await figure.$$('figcaption li a'))) {
-      console.log(await (await li.getProperty('href')).jsonValue());
-      console.log(await (await li.getProperty('innerText')).jsonValue());
+  /*
+   * フォロー新着作品の詳細ページURLを取得
+   */
+  let figureUrls:Array<string> = [];
+  for (let p = 1; p <= 1; p++) { // XXX
+    await page.goto(`https://www.pixiv.net/bookmark_new_illust.php?p=${p}`);
+
+    // 参考: https://qiita.com/go_sagawa/items/85f97deab7ccfdce53ea
+    let figures = await page.$$('#js-mount-point-latest-following figure');
+    for (let figure of figures) {
+      let li = await figure.$('figcaption li:nth-child(1) a');
+      if (!li) continue;
+
+      let figureUrl = (await (await li.getProperty('href')).jsonValue()) + '';
+      figureUrls.push(figureUrl);
     }
-    console.log('---');
-    await figure.screenshot({
+  }
+  console.log('figureUrls', figureUrls);
+
+  /*
+   * 詳細ページURLからvimagemoreへ投稿 
+   */
+  for (let url of figureUrls) {
+    console.log('TODO', url);
+    await page.goto(url);
+    const imgSelector = 'div[role=presentation] a > img';
+    await page.waitForSelector(imgSelector);
+    let img = await page.$(imgSelector);
+    if (!img) continue;
+
+    //XXX とりあえずスクショ撮影してるけどvimagemoreに投稿したい
+    await img.screenshot({
       path: 'figure.png' ,
     });
+
+    console.log('ok!');
+    break; //XXX とりあえず最初の画像だけ処理
   }
 
   await browser.close();
